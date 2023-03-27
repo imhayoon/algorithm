47d9543f27a6765dd0e77f7fdf2d8aa2f3f8562d
imhayoon <127744912+imhayoon@users.noreply.github.com>
2023-03-27 10:53:07 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.py
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.py
@@ -1,3 +1,9 @@
 def solution(num1, num2):
-    answer = 1 if num1==num2 else -1
+    answer = 0
+    
+    if (num1 == num2):
+        answer = 1
+    else:
+        answer = -1
+    
     return answer
