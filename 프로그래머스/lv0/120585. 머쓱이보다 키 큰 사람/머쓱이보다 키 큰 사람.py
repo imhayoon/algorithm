fb49a2fc786a7f4a875a5c67beb3a448aad76f50
imhayoon <127744912+imhayoon@users.noreply.github.com>
2023-03-28 11:46:07 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -1,11 +1,8 @@
 def solution(array, height):
     answer = 0
     
-    for i in range(len(array)):
-        
-        print(array[i])
+    for i in array:
+        if (i > height):
+            answer += 1
     
-        if (height < array[i]):
-            answer = answer + 1
-                
     return answer
