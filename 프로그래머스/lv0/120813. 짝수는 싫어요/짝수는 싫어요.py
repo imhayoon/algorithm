ba58fba3c3d22955c3ac40a85c83f8f8a7087137
imhayoon <127744912+imhayoon@users.noreply.github.com>
2023-03-30 12:14:39 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.py
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.py
@@ -1,7 +1,7 @@
 def solution(n):
     answer = []
-    for i in range(1, n+1, 2):
-        #print(i)
+    
+    for i in range(1,n+1,2):
         answer.append(i)
-    print(answer)
+    
     return answer
