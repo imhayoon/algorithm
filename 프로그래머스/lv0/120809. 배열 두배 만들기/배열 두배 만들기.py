82c2dc5afaba25d8d14cfd3781c4c7cced8e10a4
imhayoon <127744912+imhayoon@users.noreply.github.com>
2023-03-30 12:23:23 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.15 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.py
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.py
@@ -1,9 +1,7 @@
 def solution(numbers):
-    # 0<= ... < 5
-    answer  = []
-
+    answer = []
+    
     for i in range(len(numbers)):
-        #print(numbers[i]*2)
-        answer.append(numbers[i]*2)
+        answer.append(numbers[i] * 2)
     
     return answer
